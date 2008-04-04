5b1d484fea689e21b2e201595c1bbd5a76a15d28
Keith Duncan <keith@realmacsoftware.com>
2008-04-04 14:12:53 +0000

    Added new files

---
+++ KDRange.h
@@ -0,0 +1,11 @@
+//
+//  KDRange.h
+//  Amber
+//
+//  Created by Keith Duncan on 24/01/2008.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+extern const NSRange KDEmptyRange;
---
+++ KDRange.m
@@ -0,0 +1,11 @@
+//
+//  KDRange.m
+//  Amber
+//
+//  Created by Keith Duncan on 24/01/2008.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import "KDRange.h"
+
+const NSRange KDEmptyRange = {0, 0};
