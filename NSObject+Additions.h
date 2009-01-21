46ad4c192f2727539b12db65b4bd64618c38b75c
Jonathan Dann <j.p.dann@gmail.com>
2009-01-21 13:50:08 +0000

    Added -optionalProxy method.

--- NSObject+Additions.h
+++ NSObject+Additions.h
@@ -11,4 +11,5 @@
 @interface NSObject (AFAdditions) // Note: experimental interface
 - (id)mainThreadProxy; // Any messages sent will be performed synchronously
 - (id)threadProxy:(NSThread *)object; // Note: performed synchronously if (thread == [NSThread mainThread])
+- (id)optionalProxy; // any messages sent will be tested for respondsToSelector:
 @end
