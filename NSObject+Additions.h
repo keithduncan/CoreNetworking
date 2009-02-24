9179f67fbf433770ba60f91f2e2538ea6c7b1774
Keith Duncan <keith@realmacsoftware.com>
2009-02-24 11:37:51 +0000

    Fixed typos in the comments

--- NSObject+Additions.h
+++ NSObject+Additions.h
@@ -13,13 +13,13 @@
 // Note: experimental interface
 
 // Caution: don't become overly confident with the thread proxy methods
-//	If you intent to execute a method on the main thread using the proxy, that method will be enqued in the main thread's run loop and WILL execute on the main thread - the calling thread WILL block until the method returns - but once it does the execution will continue in the calling thread
-//	Execution doesn't yield to the main thread so be careful what you do with return values from -threadProxy
+//	If you intend to execute a method on the main thread using the proxy, that method will be enqued in the main thread's run loop and WILL execute on the main thread - the calling thread WILL block until the method returns - but once it does the execution will continue in the calling thread
+//	Execution doesn't yield to the main thread so be careful what you do with return values of methods called on -threadProxy
 
 - (id)mainThreadProxy; // Note: messages will be performed synchronously
 - (id)backgroundThreadProxy;
 - (id)threadProxy:(NSThread *)object; // Note: messages performed synchronously iff (thread == [NSThread mainThread])
 
-- (id)protocolProxy:(Protocol *)protocol; // Note: only selectors that return true for respondsToSelector: will be forwarded
+- (id)protocolProxy:(Protocol *)protocol; // Note: only selectors that return true for -respondsToSelector: will be forwarded
 
 @end
