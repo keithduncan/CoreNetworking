f78cd9e776f474701546203098cf8085294d874a
Keith Duncan <keith@realmacsoftware.com>
2009-04-04 21:51:30 +0000

    AmberFoundation: Recursive protocol lookup in the optional proxy

--- AmberFoundation/AFProtocolProxy.m
+++ AmberFoundation/AFProtocolProxy.m
@@ -35,15 +35,38 @@
 	[invocation invokeWithTarget:_target];
 }
 
-- (NSMethodSignature *)methodSignatureForSelector:(SEL)selector {	
+/*!
+	@function
+	@abstract	This is a recursive function to find a method signature in a protocol and its conformed protcols
+ */
+static NSMethodSignature *AFProtcolGetMethodSignature(SEL selector, Protocol *protocol) {
 	for (SInt8 mask = 0; mask < 4; mask++) {
-		struct objc_method_description description = protocol_getMethodDescription(_protocol, selector, (mask & 0x01), (mask & 0x02));
+		struct objc_method_description description = protocol_getMethodDescription(protocol, selector, (mask & 0x01), (mask & 0x02));
 		if (description.name == NULL && description.types == NULL) continue;
 		
 		return [NSMethodSignature signatureWithObjCTypes:description.types];
 	}
 	
-	return nil;
+	NSMethodSignature *signature = nil;
+	
+	unsigned int count = 0;
+	Protocol **conformingProtocols = protocol_copyProtocolList(_protocol, &count);
+	
+	if (count != 0) {
+		for (unsigned int currentIndex = 0; currentIndex < count; currentIndex++) {
+			Protocol *currentProtocol = conformingProtocols[curentIndex];
+			signature = AFProtcolGetMethodSignature(selector, currentProtocol);
+			if (signature != nil) break;
+		}
+	}
+	
+	free(conformingProtocols);
+	
+	return signature;
+}
+
+- (NSMethodSignature *)methodSignatureForSelector:(SEL)selector {
+	return AFProtcolGetMethodSignature(selector,_protocol);
 }
 
 @end
