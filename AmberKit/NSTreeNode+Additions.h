f4e18d21d14bb1d262414fd10a3172c1cdf918a0
Keith Duncan <keith@realmacsoftware.com>
2009-05-16 20:12:34 +0000

    AmberKit:
    Changed the NSTreeNode category methods into functions taking a self argument. This works around rdar://problem/5438559

--- AmberKit/NSTreeNode+Additions.h
+++ AmberKit/NSTreeNode+Additions.h
@@ -8,11 +8,35 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface NSTreeNode (AFAdditions)
-- (NSSet *)objectsAtIndexPaths:(NSArray *)indexPaths;
+/*
+	@header
+	@brief	These functions should really be a category on NSTreeNode, however because of an implementation
+			detail in NSTreeController it returns an object conforming to the interface of NSTreeNode but
+			not actually decending from it. These functions will work for both the private class returned from
+			NSTreeController and your own NSTreeNode based structures.
+ */
 
-- (NSMutableSet *)setFromNodeInclusive:(BOOL)inclusive;
-- (NSMutableArray *)arrayFromNodeInclusive:(BOOL)inclusive;
+/*
+	This returns a collection of nodes by concatenating the leaf of each index path to a set.
+ */
+extern NSSet *AFTreeNodeObjectsAtIndexPaths(NSTreeNode *self, NSArray *indexPaths);
 
-- (void)addChildrenToCollection:(id)collection;
-@end
+/*
+	This returns a set representation of the tree collapsed into an unordered collection.
+	This function may be faster for larege data sets where order is not important.
+	@param	|inclusive| determines whether to include the |self| node in the result.
+ */
+extern NSSet *AFTreeNodeSetFromNodeInclusive(NSTreeNode *self, BOOL inclusive);
+
+/*
+	This returns a set representation of the tree collapsed into an unordered collection.
+	@param	|inclusive| determines whether to include the |self| node in the result.
+ */
+extern NSArray *AFTreeNodeArrayFromNodeInclusive(NSTreeNode *self, BOOL inclusive);
+
+/*
+	This is a recursive function that will add the child nodes of |self| to the provided collection.
+	It accounts for creating an ordered collection by enumerating the children and then adding each of thier children in order.
+	@param	|collection| should be a mutable collection, otherwise an exception will be thrown.
+ */
+extern void AFTreeNodeAddChildrenToCollection(NSTreeNode *self, id <NSFastEnumeration> collection);
--- AmberKit/NSTreeNode+Additions.m
+++ AmberKit/NSTreeNode+Additions.m
@@ -8,79 +8,41 @@
 
 #import "NSTreeNode+Additions.h"
 
-@interface _NSControllerTreeProxy : NSObject {
-    id _controller;
-}
-
-- (NSTreeNode *)descendantNodeAtIndexPath:(NSIndexPath *)path;
-
-@end
-
-@interface _NSControllerTreeProxy (AFPrivateAdditions)
-- (void)addChildrenToCollection:(id)collection;
-@end
-
-// This function doesn't handle recursive relationships
-void AddChildrenToCollection(id node, id collection) {	
-	for (NSTreeNode *currentNode in [node childNodes]) {
-		[collection addObject:currentNode];
+/*
+	Note: this function doesn't handle recursive relationships
+ */
+static void _AFTreeNodeAddChildrenToCollection(NSTreeNode *self, id <NSFastEnumeration> collection) {
+	for (NSTreeNode *currentNode in [self childNodes]) {
+		[(id)collection addObject:currentNode];
 		
 		if ([currentNode isLeaf]) continue;
-		AddChildrenToCollection(currentNode, collection);
+		_AFTreeNodeAddChildrenToCollection(currentNode, collection);
 	}
 }
 
-id CollectionFromNode(Class collectionClass, id node, BOOL inclusive) {
+static id <NSFastEnumeration> _AFTreeNodeCollectionFromNode(NSTreeNode *self, Class collectionClass, BOOL inclusive) {
 	id collection = [[[collectionClass alloc] init] autorelease];
 	
-	if (inclusive) [collection addObject:node];
-	AddChildrenToCollection(node, collection);
+	if (inclusive) [collection addObject:self];
+	_AFTreeNodeAddChildrenToCollection(self, collection);
 	
 	return collection;
 }
 
-@implementation _NSControllerTreeProxy (AFPrivateAdditions)
-
-#warning this is a stop-gap measure until Apple fixes _NSControllerTreeProxy to inherit from NSTreeNode
-
-- (NSSet *)objectsAtIndexPaths:(NSArray *)indexPaths {
+extern NSSet *AFTreeNodeObjectsAtIndexPaths(NSTreeNode *self, NSArray *indexPaths) {
 	NSMutableSet *objects = [NSMutableSet setWithCapacity:[indexPaths count]];
 	for (NSIndexPath *currentPath in indexPaths) [objects addObject:[self descendantNodeAtIndexPath:currentPath]];
 	return objects;
 }
 
-- (NSMutableSet *)setFromNodeInclusive:(BOOL)inclusive {
-	return CollectionFromNode([NSMutableSet class], self, inclusive);
+extern NSSet *AFTreeNodeSetFromNodeInclusive(NSTreeNode *self, BOOL inclusive) {
+	return (id)_AFTreeNodeCollectionFromNode(self, [NSMutableSet class], inclusive);
 }
 
-- (NSMutableArray *)arrayFromNodeInclusive:(BOOL)inclusive {
-	return CollectionFromNode([NSMutableArray class], self, inclusive);
+extern NSArray *AFTreeNodeArrayFromNodeInclusive(NSTreeNode *self, BOOL inclusive) {
+	return (id)_AFTreeNodeCollectionFromNode(self, [NSMutableArray class], inclusive);
 }
 
-- (void)addChildrenToCollection:(id)collection {
-	AddChildrenToCollection(self, collection);
+extern void AFTreeNodeAddChildrenToCollection(NSTreeNode *self, id <NSFastEnumeration> collection) {
+	_AFTreeNodeAddChildrenToCollection(self, collection);
 }
-
-@end
-
-@implementation NSTreeNode (AFPrivateAdditions)
-
-- (NSSet *)objectsAtIndexPaths:(NSArray *)indexPaths {
-	NSMutableSet *objects = [NSMutableSet setWithCapacity:[indexPaths count]];
-	for (NSIndexPath *currentPath in indexPaths) [objects addObject:[self descendantNodeAtIndexPath:currentPath]];
-	return objects;
-}
-
-- (NSMutableSet *)setFromNodeInclusive:(BOOL)inclusive {
-	return CollectionFromNode([NSMutableSet class], self, inclusive);
-}
-
-- (NSMutableArray *)arrayFromNodeInclusive:(BOOL)inclusive {
-	return CollectionFromNode([NSMutableArray class], self, inclusive);
-}
-
-- (void)addChildrenToCollection:(id)collection {
-	AddChildrenToCollection(self, collection);
-}
-
-@end
