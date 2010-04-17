1a5a262c549fda261427312fc9f4600f074e3d08
Keith Duncan <keith@realmacsoftware.com>
2010-04-17 20:55:36 +0000

    AmberFoundation:
    Added support files.

---
+++ AmberFoundation/AmberFoundation.h
@@ -0,0 +1,59 @@
+/*!
+ *  AmberFoundation.h
+ *  AmberFoundation
+ *
+ *  Created by Keith Duncan on 27/11/2007.
+ *  Copyright 2007. All rights reserved.
+ *
+ */
+
+/*!
+	@header
+    @brief	This framework reserves the (AFAdditions) category namespace, don't use it!
+*/
+
+//
+//	Functions
+//
+
+#import "AmberFoundation/AFRange.h"
+#import "AmberFoundation/AFMacro.h"
+
+//
+//	Utility Objects
+//
+
+#import "AmberFoundation/AFUserDefaults.h"
+
+#import "AmberFoundation/AFKeyIndexedSet.h"
+#import "AmberFoundation/AFKeyIndexedArray.h"
+
+//
+//	Proxies
+//
+
+#import "AmberFoundation/AFProtocolProxy.h"
+#import "AmberFoundation/AFPriorityProxy.h"
+#import "AmberFoundation/AFKeyPathAccumulatorProxy.h"
+#import "AmberFoundation/AFTargetProxy.h"
+
+//
+//	Protocols
+//
+
+#import "AmberFoundation/AFPropertyListProtocol.h"
+
+//
+//	Categories
+//
+
+#import "AmberFoundation/NSArray+Additions.h"
+#import "AmberFoundation/NSSet+Additions.h"
+#import "AmberFoundation/NSDictionary+Additions.h"
+#import "AmberFoundation/NSObject+Metadata.h"
+#import "AmberFoundation/NSObject+Additions.h"
+#import "AmberFoundation/NSData+Additions.h"
+#import "AmberFoundation/NSDate+Additions.h"
+#import "AmberFoundation/NSSortDescriptor+Additions.h"
+#import "AmberFoundation/NSString+Additions.h"
+#import "AmberFoundation/NSBundle+Additions.h"
---
+++ AmberFoundation/AmberFoundation.m
@@ -0,0 +1,10 @@
+/*!
+ *  AmberFoundation.h
+ *  AmberFoundation
+ *
+ *  Created by Keith Duncan on 27/11/2007.
+ *  Copyright 2007. All rights reserved.
+ *
+ */
+
+#import "AmberFoundation.h"
