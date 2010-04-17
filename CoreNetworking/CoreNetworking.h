d2c1b45bf38f108f6e10fee872326331c84789f9
Keith Duncan <keith@realmacsoftware.com>
2010-04-17 20:57:10 +0000

    CoreNetworking:
    Added support files.

--- CoreNetworking/CoreNetworking.h
+++ CoreNetworking/CoreNetworking.h
@@ -8,6 +8,8 @@
 
 #import <Foundation/Foundation.h>
 
+#import "AmberFoundation/AmberFoundation.h"
+
 /*
 	Core Networking
  */
---
+++ CoreNetworking/CoreNetworking.m
@@ -0,0 +1,9 @@
+//
+//  CoreNetworking.h
+//  Amber
+//
+//  Created by Keith Duncan on 26/12/2008.
+//  Copyright 2008 software. All rights reserved.
+//
+
+#import "CoreNetworking.h"
