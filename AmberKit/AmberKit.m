2aad0fc9ca244260200e480931dd788201d39c9b
Keith Duncan <keith@realmacsoftware.com>
2012-11-25 13:50:53 +0000

    Remove AmberKit

--- AmberKit/AmberKit.m
+++
@@ -1,9 +0,0 @@
-//
-//  AmberKit.h
-//  Amber
-//
-//  Created by Keith Duncan on 15/01/2009.
-//  Copyright 2009 software. All rights reserved.
-//
-
-#import "AmberKit/AmberKit.h"
--- AmberKit/CoreAmberKit.m
+++
@@ -1,9 +0,0 @@
-//
-//  AmberKit+Additions.h
-//  Amber
-//
-//  Created by Keith Duncan on 29/03/2009.
-//  Copyright 2009. All rights reserved.
-//
-
-#import "CoreAmberKit.h"
